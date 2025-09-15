4be5aa4494bf295ecbdb769be6ad9a8bb969a4a3
Hoyoun Song <58301450+410-dev@users.noreply.github.com>
2025-09-15 10:47:11 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("application")
 }
 
 group = "me.hysong"
@@ -12,14 +13,14 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-
-//    implementation(project(":Libraries:KSFoundation"))
-//    implementation(project(":Libraries:liblks"))
-
     compileOnly("org.projectlombok:lombok:1.18.36")
     annotationProcessor("org.projectlombok:lombok:1.18.36")
     testCompileOnly("org.projectlombok:lombok:1.18.36")
     testAnnotationProcessor("org.projectlombok:lombok:1.18.36")
+}
+
+application {
+    mainClass = "org.kynesys.ksscripting.KSScriptingInterpreter"
 }
 
 tasks.test {
@@ -33,10 +34,12 @@ tasks.jar {
 
     destinationDirectory.set(file("./"))
 
-    // 3) Optional: manifest if needed by consumers
     manifest {
-        attributes["Implementation-Title"] = project.name
-        attributes["Implementation-Version"] = project.version
+        attributes(
+            "Main-Class" to "org.kynesys.ksscripting.KSScriptingInterpreter",
+            "Implementation-Title" to project.name,
+            "Implementation-Version" to project.version
+        )
     }
 }
 
